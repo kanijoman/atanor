a84b14ef725a60f968c438074fe2a9fa34ea1f27
kanijoman <111300175+kanijoman@users.noreply.github.com>
2026-09-07 10:39:08 +0200

    AT-079 Register study programme models in migrations

--- backend/migrations/env.py
+++ backend/migrations/env.py
@@ -4,6 +4,7 @@ from sqlalchemy import engine_from_config, pool
 from app.persistence.database import Base
 from app.persistence.models.requirement import Requirement
 from app.persistence.models.source import Source
+from app.persistence.models.study_programme import StudyProgramme, StudyProgrammeUnit
 
 config = context.config
 target_metadata = Base.metadata
