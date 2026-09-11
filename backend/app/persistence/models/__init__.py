a29152c724cd673726b8d77635fa6da0902db21b
kanijoman <111300175+kanijoman@users.noreply.github.com>
2026-09-11 10:33:22 +0200

    AT-087: register call persistence model

--- backend/app/persistence/models/__init__.py
+++ backend/app/persistence/models/__init__.py
@@ -1,5 +1,6 @@
+from .call import Call
 from .knowledge import Knowledge
 from .source import Source
 from .study_programme import StudyProgramme, StudyProgrammeUnit
 
-__all__ = ["Knowledge", "Source", "StudyProgramme", "StudyProgrammeUnit"]
+__all__ = ["Call", "Knowledge", "Source", "StudyProgramme", "StudyProgrammeUnit"]
